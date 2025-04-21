5c3126394b747e5c0d871e5b684ded83956e37e8
Sergey <sergey.khl.dev@gmail.com>
2025-04-20 19:57:04 -0600

    setupo pandas

---
+++ catkin_ws/build/manipulator_map/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /catkin_ws/src/manipulator_map
+# Build directory: /catkin_ws/build/manipulator_map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
